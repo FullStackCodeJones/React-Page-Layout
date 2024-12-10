63e9842456ef5979619957751889b747a434352d
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-12-10 18:17:59 -0500

    Added Component Folder and Files

---
+++ src/components/EmployeeList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EmployeeListItem from "./EmployeeListItem";
+
+function EmployeeList() {
+  return (
+    <div>
+      <EmployeeListItem />
+      <EmployeeListItem />
+      <EmployeeListItem />
+    </div>
+  );
+}
+
+export default EmployeeList;
